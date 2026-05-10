6b0af3435441af19242c5efe6eb9a3058407e512
Louis-haywood <louishaywood5@gmail.com>
2026-05-10 15:18:54 +0100

    asd

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const BACKEND_URL        = 'http://109.106.1.105:3000';
+const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1502781356709646669/XdAxneMvaernzJB7JmpE9o_H6uPh8fHfSFca6krdhf8kv4oOLxPOJF0l0iDuwIoSDiNK';
